1551d965e700d7d61c7b0b94af907ebd9b0fbbb1
Nima Shahriari <Nima.X.Shahriari@kp.org>
2020-04-14 15:11:03 -0700

    fifth

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let cloudClient = CloudClient.create("832d108c-36bb-447b-8c3b-5b183e74a3d1", "https://anylogic.prod.onprem-adf.kp.org");
+let cloudClient = CloudClient.create("832d108c-36bb-447b-8c3b-5b183e74a3d1", "https://anylogic.prod.onprem-adf.kp.org/model/908f65b5-6963-420b-86b0-6c3330aca9c0?mode=SETTINGS");
 
 let animation;
 let simulation;
